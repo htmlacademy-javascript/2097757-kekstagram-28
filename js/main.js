f60c6eb720d95649136f076a1a508d7e88457723
Pablo Wiseman <accessdevelopment@yandex.ru>
2023-03-12 00:27:48 +0500

    main.js updated

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 import createPictureStateList from './modules/data/data.js';
 import initGallery from './modules/gallery/gallery.js';
-import renderPictures from './modules/gallery/gallery.js';
-
 
 initGallery(createPictureStateList());
 
