92198eb587d8620b09dce20981a69a5e8d697b2b
Pablo Wiseman <accessdevelopment@yandex.ru>
2023-03-17 17:22:52 +0500

    updated

--- js/modules/popup/popup.js
+++ js/modules/popup/popup.js
@@ -7,35 +7,34 @@ const onDocumentKeydown = (event) => {
     /**
      * @type {HTMLButtonElement}
      */
-    const closePopup = document.querySelector('.overlay:not(.hidden) .cancel');
+    const closePopupButton = document.querySelector('.overlay:not(.hidden) .cancel');
 
-    closePopup.click();
+    closePopupButton.click();
   }
 };
 
 /**
  * A function that is called when the user clicks on the cancel button.
  * @param {MouseEvent & {target: Element}} event
- * @returns {void}
  */
-const onclosePopupClick = (event) => {
+const onClosePopupButtonClick = (event) => {
   const popup = event.target.closest('.overlay');
 
   popup.classList.add('hidden');
 
   document.body.classList.remove('modal-open');
-  document.removeEventListener('keydown', onclosePopupClick);
+  document.removeEventListener('keydown', onDocumentKeydown);
 };
 
 /**
  * @param {Element} popup
  */
 const openPopup = (popup) => {
-  const closePopup = popup.querySelector('.cancel');
+  const closePopupButton = popup.querySelector('.cancel');
 
   popup.classList.remove('hidden');
   popup.scroll(0, 0);
-  closePopup.addEventListener('click', onclosePopupClick);
+  closePopupButton.addEventListener('click', onClosePopupButtonClick);
 
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
